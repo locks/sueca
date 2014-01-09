4bda29cd4ebd9ecf334daba12421e4b5e2c90f49
Ricardo Mendes <rokusu@gmail.com>
2014-01-09 16:18:12 +0000

    Implements basic table setup.

--- src/sueca/core.clj
+++ src/sueca/core.clj
@@ -1,6 +1,21 @@
-(ns sueca.core)
+(ns sueca.core
+  (:require [sueca.deck]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn first-player
+  [players]
+  (first players))
+
+(defn setup-table
+  "Setups up the playing table. This involves dealing cards to the players."
+  [shuffled-deck]
+  {:hands []
+   :trunfo (peek shuffled-deck)
+   :players (deal-cards (reverse shuffled-deck) 4)})
+
+(defn deal-cards
+  "Deals cards to playrs"
+  [shuffled-deck number-of-players]
+  (partition (/ (count shuffled-deck) number-of-players) shuffled-deck))
+
+(setup-table play-deck)
+
