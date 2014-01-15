19e6f91a5ef711fa7b44fd883626f7e80cf9ca85
Ricardo Mendes <rokusu@gmail.com>
2014-01-15 22:22:33 +0000

    adds method to return highest card in collection

--- src/sueca/deck.clj
+++ src/sueca/deck.clj
@@ -24,3 +24,15 @@
   (let [ranks (map #(get %1 :rank) cards)]
     (reduce #(+ %1 %2)
             (map #(get points %1 0) ranks))))
+
+(defn- sort-value
+  [card trump]
+  (let [rankValue (.indexOf ranks (get card :rank))
+        trumpValue (if (= (card :suit) trump) 10 0)]
+    (+ rankValue trumpValue)))
+
+
+(defn highest-card
+  "Returns highest card from a collection, including trumps"
+  [cards trump]
+  (sort-by #(sort-value %1 trump) cards))
