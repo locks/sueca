799867755e962481c7f585462ecf17a72a9dc0db
Ricardo Mendes <rokusu@gmail.com>
2014-06-12 15:49:18 +0100

    Fixes description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sueca "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A library that implements the Sueca game logic"
+  :url "http://github.com/locks/sueca"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
